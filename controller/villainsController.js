10b12fd08bc6456da20aa9193b8f1457f03a450e
Keith March Mistler <kmistler@gmail.com>
2021-05-09 11:52:49 -0400

    all files set up and need to test

---
+++ controller/villainsController.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+const models = require('../models')
+
+const getAllVillains = async (req, res) => {
+  const villains = await models.villains.findAll()
+
+  return res.send(villains)
+}
+const getVillainSlug = async (req, res) => {
+  const villains = await models.villains.findOne({ where: { id } })
+
+  return res.send(villains)
+}
+const addNewVillain = async (req, res) => {
+  const {
+    name, movie, slug
+  } = req.body
+
+  if (!name || !movie || !slug) {
+    res.status(400).send('Must contain name, movie, and slug')
+  }
+
+  const newVillain = { name, movie, slug }
+
+  const villain = await models.villains.create(newVillain)
+
+  return res.status(201).send(villain)
+}
+
+module.exports = { getAllVillains, getVillainSlug, addNewVillain }
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
 const express = require('express')
+const { getAllVillains, getVillainSlug, addNewVillain } = require('./controller/villainsController.js')
 
 const app = express()
 
 app.set('view engine', 'pug')
 
-app.get('/villains', (req, res) => {
-  res.render('index')
-})
+app.get('/villains', getAllVillains)
+
+app.get('/villains/:slug', getVillainSlug)
+
+app.post('/teams', express.json(), addNewVillain)
 
 app.all('*', (req, res) => {
   return res.sendStatus(404)
