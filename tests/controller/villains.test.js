1012347ac21321a181e51c874511b7632e0b925a
Keith March Mistler <kmistler@gmail.com>
2021-05-13 13:08:42 -0400

    describe get all villains function test created

--- tests/controller/villains.test.js
+++ tests/controller/villains.test.js
@@ -6,7 +6,33 @@ const { getAllVillains, getVillainSlug, addNewVillain } = require('../../control
 const models = require ('../../models')
 const { villainsList, singleVillain } = require('../mocks/villains')
 const { req } = require('express')
+const { heroesList } = require('../../../super-hero-api/tests/mocks/heroes')
 
 chai.use(sinonChai)
 const { expect } = chai
+
+describe('Villains controller', () => {
+
+  let stubbedFineOne
+
+  before(() => {
+    stubbedFindOne = sinon.stub(models.villains, 'findone')
+  })
+
+  afterEach(() => {
+    stubbedFindOne.resetBehavior()
+  })
+
+  describe('Gets all villains', () => {
+    it('gets a list of villains from DB and calls response.send() with that list', async => {
+      const stubbedFindAll = sinon.stub(models.villains, 'findAll').returns(villainsList)
+      const stubbedSend = sinon.stub()
+      const response = { send: stubbedSend }
+
+      await getAllVillains({}, response)
+      expect(stubbedFindAll.to.have.callCount(1))
+      expect(stubbedSend).to.have.been.calledWith(villainsList)
+    })
+  })
+})
 
